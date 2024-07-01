97fb1e3c9ff8cdc118a45b4fb7b46320c5666825
tchoforwilson <tchoforwilson@gmail.com>
2024-07-01 03:25:18 +0100

    Various projects data

---
+++ src/data/projects.js
@@ -0,0 +1,19 @@
+const projects = [
+  {
+    img: require("../assets/images/omnifood.png"),
+    title: "Omnifood",
+    description: "A website for ordering Fast and quality food at low cost.",
+    to: "https://omnifood-tchofor.netlify.app/",
+    tools: ["CSS3", "HTML5", "JavaScript"],
+  },
+  {
+    img: require("../assets/images/nexter.png"),
+    title: "Nexter",
+    description:
+      "Nexter is a wbsite that allows family to buy thier dream homes from any country in the world with our best realtors.",
+    to: "https://nexter-wilson.netlify.app/",
+    tools: ["Sass", "HTML5", "JavaScript"],
+  },
+];
+
+export default projects;
